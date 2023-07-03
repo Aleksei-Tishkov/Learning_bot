ac1f78f9490093a0037249e351cceefb23973f27
Aleksei Tishkov <tscenic.ua@gmail.com>
2023-07-03 16:07:41 +0800

    Изменено поведение. На угрозу выключения ('Выключу тебя сейчас') отвечает провокацией

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+import re
 
 # Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
-API_TOKEN: str = 'BOT TOKEN HERE'
+with open('API.txt', 'r') as file:
+    API_TOKEN: str = file.read()
 
 # Создаем объекты бота и диспетчера
 bot: Bot = Bot(token=API_TOKEN)
 dp: Dispatcher = Dispatcher()
-
 
 # Этот хэндлер будет срабатывать на команду "/start"
 @dp.message(Command(commands=["start"]))
@@ -22,12 +23,14 @@ async def process_help_command(message: Message):
     await message.answer('Напиши мне что-нибудь и в ответ '
                          'я пришлю тебе твое сообщение')
 
-
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
 @dp.message()
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
+    if not message.text == 'Выключу тебя сейчас':
+        await message.reply(text=message.text)
+    else:
+        await message.reply('Давай, рискни')
 
 
 if __name__ == '__main__':
